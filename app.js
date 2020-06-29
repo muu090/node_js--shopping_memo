71048472e29d96b3572a63dea0d493cdee4fbfa8
Sota <sooota09@gmail.com>
2020-06-29 16:43:49 +0900

    [update] top画面/一覧画面表示

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// CSSや画像ファイルを置くフォルダを指定するコード
+app.use(express.static('public'));
 
-// サーバーを起動するコードを貼り付けてください
+
+// top画面で表示
+app.get('/',(req, res) => {
+  res.render('top.ejs');
+});
+
+// index画面表示
+app.get('/index',(req, res) => {
+  res.render('index.ejs');
+});
+
+// サーバーを起動するコード
 app.listen(3000);
 
